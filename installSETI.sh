fa20558e880cc8dd527a76d29c2895734c41d9f2
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2015-09-16 22:43:08 -0500

    initial version

---
+++ installSETI.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -x
+#
+# Install BOINC software
+#
+#
+# just compiling the client
+yum -y install git gcc-c++ autoconf openssl-devel automake libtool libcurl-devel
+
+# Clone repo
+git clone https://github.com/BOINC/boinc boinc
+
+# Build
+cd boinc
+./_autosetup
+./configure --disable-manager --enable-server
+make
+
+useradd boinc
+
+ /usr/sbin/usermod -G boinc -a username
+chmod g+rw /var/lib/boinc
+chmod g+rw /var/lib/boinc/*.*
+ln -s /var/lib/boinc/gui_rpc_auth.cfg /home/username/gui_rpc_auth.cfg
+
+
+exit;
+
+# clean-up local repo
+git clean -f -d -x
+
+
+: <<'END'
+
+# Software prerequisites
+# full package list:
+
+    git
+    libtool
+    gcc-c++
+    libstdc++-static
+    MySQL-python
+    php-mysql
+    php-gd
+    php-cli
+    openssl-devel
+    mysql-devel
+    mysql-server
+    httpd
+    php
+
+END
