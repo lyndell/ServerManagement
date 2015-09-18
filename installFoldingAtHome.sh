b29e4f51fba7d6ef8529b77ab8e2a5a4ba0e03b0
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2015-09-17 20:33:28 -0500

    inital protein install code, not expected to work as intended

--- installFoldingAtHome.sh
+++ installFoldingAtHome.sh
@@ -1,2 +1,5 @@
 wget https://fah.stanford.edu/file-releases/public/release/fahclient/centos-5.3-64bit/v7.4/fahclient-7.4.4-1.x86_64.rpm
 rpm -ivh fahclient-7.4.4-1.x86_64.rpm 
+
+# TODO: upload config
+
