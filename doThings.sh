1f2d6f4424dafa68ebbe024439d1f8673279b462
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2015-10-16 11:57:24 -0500

    generate SSH key

---
+++ doThings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+if [ ! -e ~/.ssh ]
+then
+  mkdir ~/.ssh
+fi
+
+# Generate key
+ssh-keygen -q -t rsa -b 4096 -C `hostname` -N '' -f ~/.ssh/id_rsa
+
+# Display public key
+head ~/.ssh/id_rsa.pub
+
