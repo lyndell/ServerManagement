44473cb70a161bf2a3fe739997c569360c41686f
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2015-10-16 12:00:35 -0500

    convert to function

--- doThings.sh
+++ doThings.sh
@@ -2,14 +2,18 @@
 
 set -x
 
-if [ ! -e ~/.ssh ]
-then
-  mkdir ~/.ssh
-fi
+function mkSSHkey {
+  if [ ! -e ~/.ssh ]
+  then
+    mkdir ~/.ssh
+  fi
 
-# Generate key
-ssh-keygen -q -t rsa -b 4096 -C `hostname` -N '' -f ~/.ssh/id_rsa
+  # Generate key
+  ssh-keygen -q -t rsa -b 4096 -C `hostname` -N '' -f ~/.ssh/id_rsa
 
-# Display public key
-head ~/.ssh/id_rsa.pub
+  # Display public key
+  head ~/.ssh/id_rsa.pub
+}
+
+mkSSHkey
 
