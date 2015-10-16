5841ffd6c2295cea7e32851a6b5bba26482a847b
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2015-10-16 12:11:54 -0500

    check for backup directory

--- backup.sh
+++ backup.sh
@@ -2,6 +2,23 @@
 
 set -x
 
+BACKUPPATH='/backup/'
+if [ -e $BACKUPPATH ]; then
+   echo "File '$BACKUPPATH' Exists"
+  if [ -d $BACKUPPATH ]; then
+     echo "File '$BACKUPPATH' is a directory"
+     # Good, we can work with it.
+  else
+     echo "The File '$BACKUPPATH' is NOT a directory"
+     # BAD, we canNOT work with it.
+  fi
+else
+   echo "The File '$BACKUPPATH' Does Not Exist"
+   mkdir -p $BACKUPPATH
+fi
+exit;
+
+
 timenow=`date +%s`
 backupfilename=/backup.${timenow}.tbz2
 
